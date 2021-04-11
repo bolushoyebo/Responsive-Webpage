b65e9f238cb425b50fd13979a60d3c4f0a4f30c0
bolushoyebo <53879011+bolushoyebo@users.noreply.github.com>
2021-04-11 15:11:17 +0100

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,10 @@
+const tooglebuttton = document.querySelector(".tooglebuutton")
+const navlinks = document.querySelector(".navlinks")
+
+
+tooglebuttton.addEventListener("click", show);
+
+function show() {
+    navlinks.style.;
+}
+ 
